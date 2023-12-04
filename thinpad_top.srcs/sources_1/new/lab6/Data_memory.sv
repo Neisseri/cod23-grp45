7f1b67324cf09d4263508ece3f73becf2ceef060
Neisseri <neissertong@gmail.com>
2023-12-04 15:47:13 +0800

    inst cache

--- thinpad_top.srcs/sources_1/new/lab6/Data_memory.sv
+++ thinpad_top.srcs/sources_1/new/lab6/Data_memory.sv
@@ -79,7 +79,7 @@ module Data_memory #(
     reg [DATA_WIDTH-1:0] data_out_raw;
     logic [DATA_WIDTH-1:0] data_out_shift;
 
-    //符号位拓�??
+    //符号位拓�??
     logic sign_bit;
     always_comb begin
         case (sel)
@@ -133,9 +133,13 @@ module Data_memory #(
     end
 endmodule
 
+// Instruction Cache
 module Instruction_memory #(
     parameter ADDR_WIDTH = 32,
-    parameter DATA_WIDTH = 32
+    parameter DATA_WIDTH = 32,
+    parameter CACHE_SIZE = 1024,  // 1024 instructions
+    parameter CACHE_LINE_SIZE = 32, // 32 bytes per line
+    parameter CACHE_ASSOCIATIVITY = 4  // 缓存的相联度为 4
 )(
     input wire clk,
     input wire rst,
@@ -160,12 +164,22 @@ module Instruction_memory #(
 
     input wire pipeline_stall,
     output logic idle_stall
-    );
+);
 
+    // instruction cache
+    reg [DATA_WIDTH-1:0] cache [CACHE_SIZE/CACHE_LINE_SIZE-1:0][CACHE_ASSOCIATIVITY-1:0];
+    // CACHE_SIZE/CACHE_LINE_SIZE groups
+    // CACHE_ASSOCIATIVITY rows in each group
+    reg [ADDR_WIDTH-1:0] cache_tag [CACHE_SIZE/CACHE_LINE_SIZE-1:0][CACHE_ASSOCIATIVITY-1:0];
+    // tag for each cache row: index of memory block
+    reg [CACHE_ASSOCIATIVITY-1:0] cache_valid [CACHE_SIZE/CACHE_LINE_SIZE-1:0];
+    // validity for each cache row: if the data is valid
+
+    
     typedef enum logic [3:0] {
-    STATE_READ_SRAM_ACTION,
-    STATE_DONE
-} state_t;
+        STATE_READ_SRAM_ACTION,
+        STATE_DONE
+    } state_t;
 
     state_t state;
     
@@ -179,27 +193,131 @@ module Instruction_memory #(
 
     assign idle_stall = 0;
 
+    // cache hit sign
+    reg cache_hit;
+    reg [CACHE_ASSOCIATIVITY-1:0] selected_way; // which way is selected
+
+    always_comb begin
+        // check if the cache hit
+        cache_hit = 0;
+        selected_way = 0;
+        if (mem_en) begin
+            for (int i = 0; i < CACHE_ASSOCIATIVITY; i++) begin
+                if (cache_valid[addr / CACHE_LINE_SIZE][i] && (cache_tag[addr / CACHE_LINE_SIZE][i] == addr[ADDR_WIDTH-1:ADDR_WIDTH-1-CACHE_LINE_SIZE+1])) begin
+                    cache_hit = 1;
+                    selected_way = i;
+                end
+            end
+        end
+    end
+
     always_ff @(posedge clk) begin
-        if(rst)begin
-            data_out <= `NOP_INSTR;
-            state <= STATE_DONE;
+        if (rst) begin
+            cache_tag <= '0;
+            cache_valid <= '0;
         end else begin
-            if(mem_en)begin
+            if (mem_en) begin
                 case (state)
+                
                     STATE_READ_SRAM_ACTION: begin
-                        if(wb_ack_i) begin
-                            data_out <= wb_dat_i;
+                        if (!cache_hit) begin
+                            // cache miss
+                            if (wb_ack_i) begin
+                                // update cache
+                                cache[addr / CACHE_LINE_SIZE][selected_way] <= wb_dat_i;
+                                cache_tag[addr / CACHE_LINE_SIZE][selected_way] <= addr[ADDR_WIDTH-1:ADDR_WIDTH-1-CACHE_LINE_SIZE+1];
+                                cache_valid[addr / CACHE_LINE_SIZE][selected_way] <= 1'b1;
+                                // pass data
+                                data_out <= wb_dat_i;
+                                state <= STATE_DONE;
+                            end
+                        end else begin
+                            // cache hit
+                            data_out <= cache[addr / CACHE_LINE_SIZE][selected_way];
                             state <= STATE_DONE;
                         end
                     end
+
                     STATE_DONE: begin
                         if(!pipeline_stall)begin
                                 state <= STATE_READ_SRAM_ACTION;
                         end
                     end
+
                 endcase
             end
         end
     end
 endmodule
+
+
+// module Instruction_memory #(
+//     parameter ADDR_WIDTH = 32,
+//     parameter DATA_WIDTH = 32
+// )(
+//     input wire clk,
+//     input wire rst,
+
+//     // wishbone master
+//     output logic wb_cyc_o,
+//     output logic wb_stb_o,
+//     input wire wb_ack_i,
+//     output logic [ADDR_WIDTH-1:0] wb_adr_o,
+//     output logic [DATA_WIDTH-1:0] wb_dat_o,
+//     input wire [DATA_WIDTH-1:0] wb_dat_i,
+//     output logic [DATA_WIDTH/8-1:0] wb_sel_o,
+//     output logic wb_we_o,
+
+//     output logic master_ready_o,
+//     input wire mem_en,
+//     input wire write_en,
+//     input wire [ADDR_WIDTH-1:0] addr,
+//     input wire [DATA_WIDTH-1:0] data_in,
+//     input wire [DATA_WIDTH/8-1:0] sel,
+//     output reg [DATA_WIDTH-1:0] data_out,
+
+//     input wire pipeline_stall,
+//     output logic idle_stall
+//     );
+
+//     typedef enum logic [3:0] {
+//     STATE_READ_SRAM_ACTION,
+//     STATE_DONE
+// } state_t;
+
+//     state_t state;
+    
+//     assign wb_dat_o = data_in;
+//     assign wb_adr_o = addr;
+//     assign wb_sel_o = sel;
+//     assign wb_cyc_o = (state == STATE_READ_SRAM_ACTION);
+//     assign wb_stb_o = (state == STATE_READ_SRAM_ACTION);
+//     assign master_ready_o = !wb_stb_o;
+//     assign wb_we_o = write_en;
+
+//     assign idle_stall = 0;
+
+//     always_ff @(posedge clk) begin
+//         if(rst)begin
+//             data_out <= `NOP_INSTR;
+//             state <= STATE_DONE;
+//         end else begin
+//             if(mem_en)begin
+//                 case (state)
+//                     STATE_READ_SRAM_ACTION: begin
+//                         if(wb_ack_i) begin
+//                             data_out <= wb_dat_i;
+//                             state <= STATE_DONE;
+//                         end
+//                     end
+//                     STATE_DONE: begin
+//                         if(!pipeline_stall)begin
+//                                 state <= STATE_READ_SRAM_ACTION;
+//                         end
+//                     end
+//                 endcase
+//             end
+//         end
+//     end
+// endmodule
 
