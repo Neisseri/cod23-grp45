6e813e759551e0b1f8e71db1bfc7cba837df90c7
Neisseri <neissertong@gmail.com>
2023-12-04 21:17:47 +0800

    cache hit

--- thinpad_top.srcs/sources_1/new/lab6/Data_memory.sv
+++ thinpad_top.srcs/sources_1/new/lab6/Data_memory.sv
@@ -139,8 +139,8 @@ module Instruction_memory #(
     parameter DATA_WIDTH = 32,
     parameter CACHE_SIZE = 128,  // 256 instructions
     parameter CACHE_LINE_SIZE = 32, // 32 bytes per line
-    parameter CACHE_ASSOCIATIVITY = 16,  // CACHE_ASSOCIATIVITY ways
-    parameter CACHE_GROUP_SIZE = CACHE_SIZE / CACHE_ASSOCIATIVITY // 256 / 8 = 32
+    parameter CACHE_ASSOCIATIVITY = 8,  // CACHE_ASSOCIATIVITY ways
+    parameter CACHE_GROUP_SIZE = CACHE_SIZE / CACHE_ASSOCIATIVITY // 128 / 8 = 16
 )(
     input wire clk,
     input wire rst,
@@ -173,8 +173,10 @@ module Instruction_memory #(
     // CACHE_ASSOCIATIVITY rows in each group (4)
     reg [ADDR_WIDTH-1:0] cache_tag [CACHE_GROUP_SIZE-1:0][CACHE_ASSOCIATIVITY-1:0];
     // tag for each cache row: index of memory block
-    reg cache_valid [CACHE_GROUP_SIZE-1:0][CACHE_ASSOCIATIVITY-1:0];
-    // validity for each cache row: if the data is valid
+    reg [$clog2(CACHE_ASSOCIATIVITY)-1:0] cache_group_num [CACHE_GROUP_SIZE-1:0];
+    // number of valid rows in group
+
+    reg [$clog2(CACHE_GROUP_SIZE)-1:0] group_index;
     
     typedef enum logic [3:0] {
         STATE_READ_SRAM_ACTION,
@@ -201,9 +203,10 @@ module Instruction_memory #(
         // check if the cache hit
         cache_hit = 0;
         selected_way = 0;
+        group_index = ((addr - 32'h8000_0000) / 4 / CACHE_ASSOCIATIVITY) % CACHE_GROUP_SIZE;
         if (mem_en) begin
             for (int i = 0; i < CACHE_ASSOCIATIVITY; i++) begin
-                if (cache_valid[addr / CACHE_GROUP_SIZE][i] && (cache_tag[addr / CACHE_GROUP_SIZE][i] == addr)) begin
+                if (cache_tag[group_index][i] == addr) begin
                     cache_hit = 1;
                     selected_way = i;
                 end
@@ -224,8 +227,8 @@ module Instruction_memory #(
                 end
             end
             for (int i = 0; i < CACHE_GROUP_SIZE; i = i + 1) begin
-                for (int j = 0; j < CACHE_ASSOCIATIVITY; j = j + 1) begin
-                    cache_valid[i][j] = 0;
+                for (int j = 0; j < $clog2(CACHE_ASSOCIATIVITY); j = j + 1) begin
+                    cache_group_num[i][j] = 0;
                 end
             end
             data_out <= `NOP_INSTR;
@@ -239,16 +242,19 @@ module Instruction_memory #(
                             // cache miss
                             if (wb_ack_i) begin
                                 // update cache
-                                cache[addr / CACHE_GROUP_SIZE][selected_way] <= wb_dat_i;
-                                cache_tag[addr / CACHE_GROUP_SIZE][selected_way] <= addr;
-                                cache_valid[addr / CACHE_GROUP_SIZE][selected_way] <= 1'b1;
+                                cache_group_num[group_index] <= cache_group_num[group_index] + 1;
+                                if (cache_group_num[group_index] == CACHE_ASSOCIATIVITY + 1) begin
+                                    cache_group_num[group_index] <= 1;
+                                end
+                                cache[group_index][cache_group_num[group_index] - 1] <= wb_dat_i;
+                                cache_tag[group_index][cache_group_num[group_index] - 1] <= addr;
                                 // pass data
                                 data_out <= wb_dat_i;
                                 state <= STATE_DONE;
                             end
                         end else begin
                             // cache hit
-                            data_out <= cache[addr / CACHE_GROUP_SIZE][selected_way];
+                            data_out <= cache[group_index][selected_way];
                             state <= STATE_DONE;
                         end
                     end
