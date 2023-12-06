e2d35bb161985f1c3796aa00768f93bb93231bdb
xzythu <1159108732@qq.com>
2023-12-06 19:58:43 +0800

    add satp csr reg

--- thinpad_top.srcs/sources_1/new/lab6/CSR_reg.sv
+++ thinpad_top.srcs/sources_1/new/lab6/CSR_reg.sv
@@ -26,6 +26,8 @@ module CSR_reg #(
     input wire [DATA_WIDTH-1:0] csr_mie_i,
     input wire csr_mie_we_i,
 
+    output reg [DATA_WIDTH-1:0] csr_satp_o,
+
     output reg [DATA_WIDTH-1:0] csr_mstatus_o,
     output reg [DATA_WIDTH-1:0] csr_mtvec_o,
     output reg [DATA_WIDTH-1:0] csr_mepc_o,
@@ -37,6 +39,8 @@ module CSR_reg #(
     input wire priv_level_we_i,
     output reg [1:0] priv_level_o
     );
+
+    reg [DATA_WIDTH-1:0] satp;
     
     reg [DATA_WIDTH-1:0] mstatus;
     reg [DATA_WIDTH-1:0] mtvec;
@@ -50,6 +54,8 @@ module CSR_reg #(
     always_comb begin
         case(csr_adr_i)
              // TODO: correct reset value
+            12'h180: csr_o = satp;
+
             12'h300: csr_o = mstatus;
             12'h304: csr_o = mie;
             12'h305: csr_o = mtvec;
@@ -58,6 +64,8 @@ module CSR_reg #(
             12'h342: csr_o = mcause;
             12'h344: csr_o = mip;
         endcase
+
+        csr_satp_o = satp;
 
         csr_mstatus_o = mstatus;
         csr_mtvec_o = mtvec;
@@ -72,6 +80,8 @@ module CSR_reg #(
     always_ff @(posedge clk) begin
         if(rst) begin
             priv_level <= `PRIV_M_LEVEL;
+            satp <= 0;
+
             mstatus <= 0;
             mtvec <= 0;
             mscratch <= 0;
@@ -82,6 +92,8 @@ module CSR_reg #(
         end else begin
             if (csr_we_i) begin
                 case(csr_wadr_i)
+                    12'h180: satp <= csr_wdat_i;
+
                     12'h300: mstatus <= csr_wdat_i;
                     12'h304: mie <= csr_wdat_i;
                     12'h305: mtvec <= csr_wdat_i;
