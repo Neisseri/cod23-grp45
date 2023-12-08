acb89bdf6f402560c02d2eaf0c0101910f1ae269
xzythu <1159108732@qq.com>
2023-12-08 17:13:52 +0800

    add csr_regs/ add csr instr

--- thinpad_top.srcs/sources_1/new/lab6/CSR_reg.sv
+++ thinpad_top.srcs/sources_1/new/lab6/CSR_reg.sv
@@ -39,30 +39,55 @@ module CSR_reg #(
     input wire priv_level_we_i,
     output reg [1:0] priv_level_o
     );
-
-    reg [DATA_WIDTH-1:0] satp;
     
-    reg [DATA_WIDTH-1:0] mstatus;
+    reg [DATA_WIDTH-1:0] satp;
+    reg [DATA_WIDTH-1:0] sstatus; // TODO
+    reg [DATA_WIDTH-1:0] sepc; // TODO
+    reg [DATA_WIDTH-1:0] scause; // TODO
+    reg [DATA_WIDTH-1:0] stval; // TODO
+    reg [DATA_WIDTH-1:0] stvec; // TODO
+    reg [DATA_WIDTH-1:0] sscratch; // TODO
+    reg [DATA_WIDTH-1:0] sie; // TODO
+    reg [DATA_WIDTH-1:0] sip; // TODO
+    
+    reg [DATA_WIDTH-1:0] mstatus; // TODO
     reg [DATA_WIDTH-1:0] mtvec;
     reg [DATA_WIDTH-1:0] mscratch;
     reg [DATA_WIDTH-1:0] mepc;
     reg [DATA_WIDTH-1:0] mcause;
-    reg [DATA_WIDTH-1:0] mie;
-    reg [DATA_WIDTH-1:0] mip;
+    reg [DATA_WIDTH-1:0] mie; // TODO
+    reg [DATA_WIDTH-1:0] mip; // TODO
+    reg [DATA_WIDTH-1:0] mhartid; // TODO
+    reg [DATA_WIDTH-1:0] mideleg; // TODO
+    reg [DATA_WIDTH-1:0] medeleg; // TODO
+    reg [DATA_WIDTH-1:0] mtval; // TODO
+
     reg [1:0] priv_level;
     
     always_comb begin
         case(csr_adr_i)
-             // TODO: correct reset value
+            12'h100: csr_o = sstatus;
+            12'h104: csr_o = sie;
+            12'h105: csr_o = stvec;
+            12'h140: csr_o = sscratch;
+            12'h141: csr_o = sepc;
+            12'h142: csr_o = scause;
+            12'h143: csr_o = stval;
+            12'h144: csr_o = sip;
             12'h180: csr_o = satp;
 
             12'h300: csr_o = mstatus;
+            12'h302: csr_o = medeleg;
+            12'h303: csr_o = mideleg;
             12'h304: csr_o = mie;
             12'h305: csr_o = mtvec;
             12'h340: csr_o = mscratch;
             12'h341: csr_o = mepc;
             12'h342: csr_o = mcause;
+            12'h343: csr_o = mtval;
             12'h344: csr_o = mip;
+            12'hf14: csr_o = mhartid;
+            default: csr_o = 0;
         endcase
 
         csr_satp_o = satp;
@@ -81,6 +106,14 @@ module CSR_reg #(
         if(rst) begin
             priv_level <= `PRIV_M_LEVEL;
             satp <= 0;
+            sstatus <= 0;
+            sepc <= 0;
+            scause <= 0;
+            stval <= 0;
+            stvec <= 0;
+            sscratch <= 0;
+            sie <= 0;
+            sip <= 0;
 
             mstatus <= 0;
             mtvec <= 0;
@@ -89,18 +122,34 @@ module CSR_reg #(
             mcause <= 0;
             mie <= 0;
             mip <= 0;
+            mhartid <= 0;
+            mideleg <= 0;
+            medeleg <= 0;
+            mtval <= 0;
         end else begin
             if (csr_we_i) begin
                 case(csr_wadr_i)
+                    12'h100: sstatus <= csr_wdat_i;
+                    12'h104: sie <= csr_wdat_i;
+                    12'h105: stvec <= csr_wdat_i;
+                    12'h140: sscratch <= csr_wdat_i;
+                    12'h141: sepc <= csr_wdat_i;
+                    12'h142: scause <= csr_wdat_i;
+                    12'h143: stval <= csr_wdat_i;
+                    12'h144: sip <= csr_wdat_i;
                     12'h180: satp <= csr_wdat_i;
 
                     12'h300: mstatus <= csr_wdat_i;
+                    12'h302: medeleg <= csr_wdat_i;
+                    12'h303: mideleg <= csr_wdat_i;
                     12'h304: mie <= csr_wdat_i;
                     12'h305: mtvec <= csr_wdat_i;
                     12'h340: mscratch <= csr_wdat_i;
                     12'h341: mepc <= csr_wdat_i;
                     12'h342: mcause <= csr_wdat_i;
+                    12'h343: mtval <= csr_wdat_i;
                     12'h344: mip <= csr_wdat_i;
+                    12'hf14: mhartid <= csr_wdat_i;
                 endcase
             end else begin
                 if (csr_mstatus_we_i) begin
