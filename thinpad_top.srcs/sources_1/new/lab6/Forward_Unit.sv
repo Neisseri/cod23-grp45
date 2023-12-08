bebb6ab3880e6eb86a31464982d89b5b3f9b74b9
Neisseri <neissertong@gmail.com>
2023-12-08 18:21:21 +0800

    mem hazard

--- thinpad_top.srcs/sources_1/new/lab6/Forward_Unit.sv
+++ thinpad_top.srcs/sources_1/new/lab6/Forward_Unit.sv
@@ -187,9 +187,13 @@ module Forward_Unit #(
         rs1_forward_dat_o = 0;
         alu_mux_a = id_exe_alu_mux_a;
         alu_a_forward = 0;
-        if (use_mem_dat_a) begin // hazard 4 5 6
-            // TODO: hazard 4 5 6 different
-        end else if (hazard1_a) begin // hazard 1
+        if (hazard5_a) begin // hazard 5
+            rs1_forward_o = 1;
+            rs1_forward_dat_o = mem_wb_dat;
+        end else if (hazard6_a) begin // hazard 6
+            rs1_forward_o = 1;
+            rs1_forward_dat_o = wb_dat;
+        end if (hazard1_a) begin // hazard 1
             rs1_forward_o = 1;
             rs1_forward_dat_o = id_exe_dat;
         end else if (hazard2_a) begin // hazard 2
@@ -204,8 +208,12 @@ module Forward_Unit #(
         rs2_forward_dat_o = 0;
         alu_mux_b = id_exe_alu_mux_b;
         alu_b_forward = 0;
-        if (use_mem_dat_b) begin // hazard 4 5 6
-            // TODO: hazard 3 and 4 different    
+        if (hazard5_b) begin // hazard 5
+            rs2_forward_o = 1;
+            rs2_forward_dat_o = mem_wb_dat;
+        end else if (hazard6_b) begin // hazard 6
+            rs2_forward_o = 1;
+            rs2_forward_dat_o = wb_dat;
         end else if (hazard1_b) begin // hazard 1
             rs2_forward_o = 1;
             rs2_forward_dat_o = id_exe_dat;
@@ -238,16 +246,8 @@ module Forward_Unit #(
         end
     end
 
-    // test
     always_comb begin
-        if (
-            // id_exe_rf_wen && (if_id_rs1 == id_exe_rd || if_id_rs2 == id_exe_rd) && id_exe_rd != 0 || // ID -> EXE
-            // exe_mem_rf_wen && (if_id_rs1 == exe_mem_rd || if_id_rs2 == exe_mem_rd) && exe_mem_rd != 0 || // EXE -> MEM
-            // wb_rf_we && (if_id_rs1 == mem_wb_rd || if_id_rs2 == mem_wb_rd) && mem_wb_rd != 0 // MEM -> WB
-            //hazard1_a || hazard1_b || hazard2_a || hazard2_b || hazard3_a || hazard3_b ||
-            // hazard3_a || hazard3_b ||
-            hazard4_a || hazard4_b || hazard5_a || hazard5_b || hazard6_a || hazard6_b 
-        ) begin
+        if (hazard4_a || hazard4_b) begin
             exe_stall_req = 1;
         end else begin
             exe_stall_req = 0;
@@ -256,141 +256,8 @@ module Forward_Unit #(
 
     // TODO: branch hazard
     always_comb begin
-        // -------------------------------------
-        // alu_mux_a = id_exe_alu_mux_a;
-        // alu_a_forward = 0;
-        // alu_mux_b = id_exe_alu_mux_b;
-        // alu_b_forward = 0;
-        // pass_use_mem_dat_a = 0;
-        // pass_use_mem_dat_b = 0;
-        // --------------------------------------
         branch_rs1 = 0;
         branch_rs2 = 0;
     end
 
 endmodule
-
-// module Forward_Unit #(
-//     parameter ADDR_WIDTH = 32,
-//     parameter DATA_WIDTH = 32
-// )(
-//     // EXE hazard
-//     input wire [4:0] id_exe_rs1,
-//     input wire [4:0] id_exe_rs2,
-//     input wire [4:0] exe_mem_rd,
-//     input wire exe_mem_rf_wen,
-
-//     input wire [DATA_WIDTH-1:0] exe_mem_dat,
-
-//     // MEM hazard
-//     input wire [4:0] if_id_rs1,
-//     input wire [4:0] if_id_rs2,
-//     input wire [4:0] id_exe_rd,
-//     input wire exe_is_load,
-
-//     input wire use_mem_dat_a,
-//     input wire use_mem_dat_b,
-//     input wire [DATA_WIDTH-1:0] mem_wb_dat,
-
-//     input wire [2:0] id_exe_alu_mux_a,
-//     input wire [2:0] id_exe_alu_mux_b,
-
-//     output logic [2:0] alu_mux_a,
-//     output logic [2:0] alu_mux_b,
-//     output logic [DATA_WIDTH-1:0] alu_a_forward,
-//     output logic [DATA_WIDTH-1:0] alu_b_forward,
-
-//     //output logic exe_stall_req,
-//     output logic pass_use_mem_dat_a,
-//     output logic pass_use_mem_dat_b,
-
-//     output logic branch_rs1,
-//     output logic branch_rs2
-//     );
-
-//     always_comb begin
-//         alu_mux_a = id_exe_alu_mux_a;
-//         alu_a_forward = 0;
-//         alu_mux_b = id_exe_alu_mux_b;
-//         alu_b_forward = 0;
-
-//         pass_use_mem_dat_a = 0;
-//         pass_use_mem_dat_b = 0;
-//         branch_rs1 = 0;
-//         branch_rs2 = 0;
-//     end
-
-//     // logic branch_hazard_a;
-//     // logic branch_hazard_b;
-//     // assign branch_hazard_a = id_exe_rd != 0 && id_exe_rd == if_id_rs1;
-//     // assign branch_hazard_b = id_exe_rd != 0 && id_exe_rd == if_id_rs2;
-
-//     // always_comb begin
-//     //     if(branch_hazard_a)begin
-//     //         branch_rs1 = 1;
-//     //     end else begin
-//     //         branch_rs1 = 0;
-//     //     end
-
-//     //     if(branch_hazard_b)begin
-//     //         branch_rs2 = 1;
-//     //     end else begin
-//     //         branch_rs2 = 0;
-//     //     end
-//     // end
-
-//     // always_comb begin
-//     //     if(use_mem_dat_a)begin
-//     //         alu_mux_a = `ALU_MUX_FORWARD;
-//     //         alu_a_forward = mem_wb_dat;
-//     //     end else begin
-//     //         if(exe_mem_rd == id_exe_rs1 && exe_mem_rd != 0 && exe_mem_rf_wen)begin
-//     //             alu_mux_a = `ALU_MUX_FORWARD;
-//     //             alu_a_forward = exe_mem_dat;
-//     //         end else begin
-//     //             alu_mux_a = id_exe_alu_mux_a;
-//     //             alu_a_forward = 0;
-//     //         end
-//     //     end
-
-//     //     if(use_mem_dat_b)begin
-//     //         alu_mux_b = `ALU_MUX_FORWARD;
-//     //         alu_b_forward = mem_wb_dat;
-//     //     end else begin
-//     //         if(exe_mem_rd == id_exe_rs2 && exe_mem_rd != 0 && exe_mem_rf_wen)begin
-//     //             alu_mux_b = `ALU_MUX_FORWARD;
-//     //             alu_b_forward = exe_mem_dat;
-//     //         end else begin
-//     //             alu_mux_b = id_exe_alu_mux_b;
-//     //             alu_b_forward = 0;
-//     //         end
-//     //     end
-//     // end
-
-//     // logic mem_hazard_a;
-//     // logic mem_hazard_b;
-//     // assign mem_hazard_a = exe_is_load && id_exe_rd != 0 && id_exe_rd == if_id_rs1;
-//     // assign mem_hazard_b = exe_is_load && id_exe_rd != 0 && id_exe_rd == if_id_rs2;
-//     // //assign mem_hazard_a = id_exe_rd != 0 && id_exe_rd == if_id_rs1;
-//     // //assign mem_hazard_b = id_exe_rd != 0 && id_exe_rd == if_id_rs2;
-
-//     // always_comb begin
-//     //     if(mem_hazard_a || mem_hazard_b)begin
-//     //         exe_stall_req = 1;
-//     //     end else begin
-//     //         exe_stall_req = 0;
-//     //     end
-
-//     //     if(mem_hazard_a)begin
-//     //         pass_use_mem_dat_a = 1;
-//     //     end else begin
-//     //         pass_use_mem_dat_a = 0;
-//     //     end
-
-//     //     if(mem_hazard_b)begin
-//     //         pass_use_mem_dat_b = 1;
-//     //     end else begin
-//     //         pass_use_mem_dat_b = 0;
-//     //     end
-//     // end
-// endmodule
